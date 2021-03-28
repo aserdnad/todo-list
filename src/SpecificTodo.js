81e9aa7f6470d1d2471e8842938991e41971dff7
aserdnad <andreasgamardo@gmail.com>
2021-03-28 23:55:33 +0200

    listo ya agregue casi todo lo que se me habia ocurrido

--- src/SpecificTodo.js
+++ src/SpecificTodo.js
@@ -1,17 +1,26 @@
-import {useParams} from 'react-router-dom';
+import {useParams, useHistory} from 'react-router-dom';
 import useFetch from './useFetch';
+
 
 const SpecificTodo = () => {
 
+    const history = useHistory();
     const {id} = useParams();
     const {data} = useFetch('http://localhost:8000/lista/'+id);
-    console.log(data);
+    const eliminar = () =>{
+        fetch('http://localhost:8000/lista/'+id,{
+            method:'DELETE'
+        }).then(()=>{
+            history.push('/');
+        })
+    }
     return (
         <div className="todos">
             <div className="mantener">
-                <div className="todo-lista" >
-                     <h2 className="h2-todo">{data && data.titulo}</h2>
-                    <p className="p-todo">{data && data.contenido}</p>
+                <div className="todo-lista-esp">
+                     <h2 className="h2-todo-esp"><i>Titutlo: </i>  {data && data.titulo}</h2>
+                    <p className="p-todo-esp"><i>Por hacer: </i>  {data && data.contenido}</p>
+                    <button className="eliminar-todo" onClick={eliminar}>Eliminar</button>
                 </div>
             </div>   
         </div>
